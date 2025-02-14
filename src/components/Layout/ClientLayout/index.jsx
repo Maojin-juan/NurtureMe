50b7e7400a129a5cfcde8a396c56a40eb09ad0bb
Maojin-juan <edwardelric199887@gmail.com>
2025-02-14 08:50:27 +0800

    fix: 修正路由切換頁面捲軸問題
    - 新增 ScrollToTop 元件
    - 整合至 Layout 元件
    - 改善頁面切換的使用者體驗

--- src/components/Layout/ClientLayout/index.jsx
+++ src/components/Layout/ClientLayout/index.jsx
@@ -1,10 +1,12 @@
 import PropTypes from "prop-types";
 import Header from "./Header";
 import Footer from "./Footer";
+import ScrollToTop from "../ScrollToTop";
 
 const Layout = ({ children }) => {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <div>{children}</div>
       <Footer />
