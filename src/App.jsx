6f40015773f2061a48f608ed0462acc9a46af0d3
Maojin-juan <edwardelric199887@gmail.com>
2025-03-04 10:02:23 +0800

    refactor(router): 整合路由配置
    - 統一交由 router.jsx 處理

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,6 @@ import { HelmetProvider } from "react-helmet-async";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import router from "./router";
-import Layout from "@/components/Layout/ClientLayout";
-import Home from "./pages/Home";
-import Donation from "./pages/Donation";
-import Adoption from "./pages/Adoption/index";
-import News from "./pages/News";
-import About from "./pages/About";
 
 function App() {
   return (
